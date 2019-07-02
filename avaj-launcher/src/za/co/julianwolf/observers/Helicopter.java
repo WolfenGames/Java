44929f5b8de12ff41eabd99a9325087b29041bfc
jwolf <jwolf@student.wethinkcode.co.za>
2019-07-02 21:32:59 +0200

    auto commit

--- avaj-launcher/src/za/co/julianwolf/observers/Helicopter.java
+++ avaj-launcher/src/za/co/julianwolf/observers/Helicopter.java
@@ -24,17 +24,36 @@ public class Helicopter extends Aircraft implements Flyable
 	@Override
 	public void updateConditions() 
 	{
+		Coordinates newCoordinates = new Coordinates(0, 0, 0);
 		String condition = weatherTower.getWeather(coordinates);
 		switch (condition)
 		{
 			case "RAIN":
+				newCoordinates.longitude = this.coordinates.getLongitude();
+				newCoordinates.latitude = this.coordinates.getLatitude() + 1;
+				newCoordinates.height = this.coordinates.getHeight();
 				break;
 			case "SUN":
+				newCoordinates.longitude = this.coordinates.getLongitude();
+				newCoordinates.latitude = this.coordinates.getLatitude() + 10;
+				newCoordinates.height = this.coordinates.getHeight() + 2;
 				break;
 			case "SNOW":
+				newCoordinates.longitude = this.coordinates.getLongitude();
+				newCoordinates.latitude = this.coordinates.getLatitude();
+				newCoordinates.height = this.coordinates.getHeight() - 7;
 				break;
 			case "FOG":
+				newCoordinates.longitude = this.coordinates.getLongitude();
+				newCoordinates.latitude = this.coordinates.getLatitude() + 1;
+				newCoordinates.height = this.coordinates.getHeight();
 				break;
 		}
+		if (newCoordinates.height > 100) newCoordinates.height = 100;
+		if (newCoordinates.height < 1)
+		{
+			this.weatherTower.unregister(this);
+		}
+		this.coordinates = newCoordinates;
 	}
 }
