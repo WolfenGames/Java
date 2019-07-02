a72d26a1ae96b7fecc6a10dd13e7d985cc10b166
jwolf <jwolf@student.wethinkcode.co.za>
2019-07-02 08:11:06 +0200

    auto commit

--- avaj-launcher/src/za/co/julianwolf/Simulator.java
+++ avaj-launcher/src/za/co/julianwolf/Simulator.java
@@ -5,5 +5,11 @@ public class Simulator
 	public static void main(String[] args) {
 		if (args.length == 0)
 			return;
+		Parse(args[0]);
+	}
+
+	private static void Parse(String file)
+	{
+		System.console().printf("FILE :: %s", file);
 	}
 }
